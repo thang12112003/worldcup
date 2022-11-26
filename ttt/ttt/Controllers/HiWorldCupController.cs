a6e755df0c84ff387bff0f2631cc99fd824c7a81
thang12112003 <94966246+thang12112003@users.noreply.github.com>
2022-11-26 10:51:46 +0700

    worldcup

---
+++ ttt/ttt/Controllers/HiWorldCupController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ttt.Controllers
+{
+    public class HiWorldCupController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Result(int doi1, int doi2, int matran)
+        {
+            ViewData["doi1"] = "result  :" + doi1;
+            ViewData["doi2"] = doi2;
+            ViewData["matran"] = matran;
+            return View();
+        }
+    }
+}
